8a2485050fa5e72262b9e7bc8236b3b88c4a1b2b
shivangijb <37670360+shivangijb@users.noreply.github.com>
2020-11-16 22:03:21 +0530

    button added

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,13 @@ function App() {
       <div className="main">
         
         <button id="click" onClick={handleClick}></button>
-        {message &&  <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      {message && (
+        <p id="para">
+          {
+            "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+          }
+        </p>
+      )}
        
       </div>
     
