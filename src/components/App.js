b2705968428f626b40e808ab16e4844297606c72
shivangijb <37670360+shivangijb@users.noreply.github.com>
2020-11-16 21:58:29 +0530

    Changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,23 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
-export default function App() {
+function App() {
   let [message , setMessage] = React.useState(false);
   const handleClick = () => {
-    message = true;
-    setMessage(message);
+    setMessage(true);
   };
 
-  if(message){
+  
     
     return (
       <div className="main">
-        // Do not alter the main div
+        
         <button id="click" onClick={handleClick}></button>
-        <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+        {message &&  <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+       
       </div>
+    
+  
     );
-  }
-  return (
-    <div className="main">
-      // Do not alter the main div
-      <button id="click" onClick={handleClick}></button>
-    </div>
-  );
 };
+export default App;
